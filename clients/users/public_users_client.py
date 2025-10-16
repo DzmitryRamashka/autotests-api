b6450e3160bfcbfde193490e0d8c72a444808d7c
DzmitryRamashka <DzmitryRamashka@github.com>
2025-10-16 23:24:08 +0300

    Знакомство с API клиентами

---
+++ clients/users/public_users_client.py
@@ -0,0 +1,33 @@
+from clients.api_client import APIClient
+
+from httpx import Response
+
+from typing import TypedDict
+
+class UsersRequestDictionary(TypedDict):
+    """
+    Описание структуры запроса для создания пользователя
+    """
+    email: str
+    password: str
+    lastName: str
+    firstName: str
+    middleName: str
+
+class PublicUsersClient(APIClient):
+    """
+    Клиент для работы с /api/v1/users
+    """
+
+    def create_user_api(self, request: UsersRequestDictionary) -> Response:
+        """
+        Метод создаёт пользователя
+
+        :param request: Словарь с email, password, lastName, firstName, middleName
+        :return: Ответ от сервера в виде объекта httpx.Response
+        """
+        return self.post("/api/v1/users", json=request)
+
+
+
+
